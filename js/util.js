e6fa7d0699a1cb26e68178b616f2027478a68cc5
Nuno Das Neves <ndasneves@gmail.com>
2022-12-14 21:05:33 -0800

    reorganize vector functions withing utils.js

--- js/util.js
+++ js/util.js
@@ -1,14 +1,25 @@
 
+/*
+ * ##############
+ * 2d Vectors
+ * ##############
+ */
+
+// Functions that return a new vector object
+
+export function vec(x = 0, y = 0)
+{
+    return { x, y };
+}
+
+export function vecClone(v)
+{
+    return { x: v.x, y: v.y };
+}
+
 export function vecAdd(v1, v2)
 {
     return { x: v1.x + v2.x, y: v1.y + v2.y };
-}
-
-export function vecAddTo(v1, v2)
-{
-    v1.x += v2.x;
-    v1.y += v2.y;
-    return v1;
 }
 
 export function vecSub(v1, v2)
@@ -16,28 +27,9 @@ export function vecSub(v1, v2)
     return { x: v1.x - v2.x, y: v1.y - v2.y };
 }
 
-export function vecSubFrom(v1, v2)
-{
-    v1.x -= v2.x;
-    v1.y -= v2.y;
-    return v1;
-}
-
 export function vecMul(v, f)
 {
     return { x: v.x * f, y: v.y * f };
-}
-
-export function vecMulBy(v, f)
-{
-    v.x *= f;
-    v.y *= f;
-    return v;
-}
-
-export function vecLen(v)
-{
-    return Math.sqrt(v.x * v.x + v.y * v.y);
 }
 
 export function vecNorm(v)
@@ -47,7 +39,59 @@ export function vecNorm(v)
         console.error("Tried to divide by 0");
         return { x: 0, y: 0 };
     }
-    return { x: v.x/len, y: v.y/len }
+    const f = 1/len;
+    return { x: v.x * f, y: v.y * f };
+}
+
+export function vecTangentRight(v)
+{
+    return {
+        x: v.y,
+        y: -v.x
+    };
+}
+
+export function vecRand()
+{
+    const v = vec(Math.random()-0.5, Math.random()-0.5);
+    return vecNorm(v);
+}
+
+// In-place functions, prefer these whenever possible to avoid creating a new object
+
+export function vecClear(v)
+{
+    v.x = 0;
+    v.y = 0;
+    return v;
+}
+
+export function vecCopyTo(v1, v2)
+{
+    v1.x = v2.x;
+    v1.y = v2.y;
+    return v1;
+}
+
+export function vecAddTo(v1, v2)
+{
+    v1.x += v2.x;
+    v1.y += v2.y;
+    return v1;
+}
+
+export function vecSubFrom(v1, v2)
+{
+    v1.x -= v2.x;
+    v1.y -= v2.y;
+    return v1;
+}
+
+export function vecMulBy(v, f)
+{
+    v.x *= f;
+    v.y *= f;
+    return v;
 }
 
 export function vecNormalize(v)
@@ -63,14 +107,6 @@ export function vecNormalize(v)
     }
 
     return v;
-}
-
-export function vecTangentRight(v)
-{
-    return {
-        x: v.y,
-        y: -v.x
-    };
 }
 
 export function vecSetMag(v, mag)
@@ -89,35 +125,27 @@ export function vecClampMag(v, min, max)
     return vecMulBy(v, clampedLen/len);
 }
 
-export function vecClone(v)
-{
-    return { x: v.x, y: v.y };
-}
-
-export function vecCopyTo(v1, v2)
-{
-    v1.x = v2.x;
-    v1.y = v2.y;
-    return v1;
-}
-
-export function vecClear(v)
-{
-    v.x = 0;
-    v.y = 0;
-    return v;
-}
-
-export function vec(x = 0, y = 0)
-{
-    return { x, y };
-}
-
 export function vecNegate(v)
 {
     v.x = -v.x;
     v.y = -v.y;
     return v;
+}
+
+export function vecRotateBy(v, a)
+{
+    const x = v.x*Math.cos(a) - v.y*Math.sin(a);
+    const y = v.x*Math.sin(a) + v.y*Math.cos(a);
+    v.x = x;
+    v.y = y;
+    return v;
+}
+
+// Vector to scalar and scalar to vector functions
+
+export function vecLen(v)
+{
+    return Math.sqrt(v.x * v.x + v.y * v.y);
 }
 
 export function vecDot(v1, v2)
@@ -141,21 +169,6 @@ export function vecFromAngle(a)
         x: Math.cos(a),
         y: Math.sin(a)
     }
-}
-
-export function vecRotateBy(v, a)
-{
-    const x = v.x*Math.cos(a) - v.y*Math.sin(a);
-    const y = v.x*Math.sin(a) + v.y*Math.cos(a);
-    v.x = x;
-    v.y = y;
-    return v;
-}
-
-export function vecRand()
-{
-    const v = vec(Math.random()-0.5, Math.random()-0.5);
-    return vecNorm(v);
 }
 
 export function getDist(p1, p2)
