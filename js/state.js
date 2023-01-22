499712be383d4e182c8a54347245a377fee2af9f
Nuno Das Neves <ndasneves@gmail.com>
2023-01-22 15:06:40 +1100

    fixup! remove staticD functionality and mouse control for it

--- js/state.js
+++ js/state.js
@@ -369,7 +369,6 @@ function addPlayer(name, controller, pos, team, colorIdx)
         upgradeLevels: Object.fromEntries(Object.values(upgrades).map(({ id }) => [id, -1])),
         unitCds: Object.fromEntries(Object.values(UNIT).map(id => [id, 0])),
         unitUnlocked: Object.fromEntries(Object.values(units).map(({ id, needsUnlock }) => [id, needsUnlock ? false : true])),
-        staticDCd: 0,
         botState: {
             actionTimer: 0,
             strategy: BOT.AGGRO,
